7e1d408d63b2f42b1d8b9342fdd528f1825960aa
JanssenBenedict <janssen.benedict@gmail.com>
2024-08-28 16:32:21 +0700

    Update settings.py

--- mental_health_tracker/settings.py
+++ mental_health_tracker/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-*dgkn!%$4*t8jvke4!uq&%2!&gb_bx_wze13(jg2)a+)@$o8a!
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ["localhost", "127.0.0.1"]
+ALLOWED_HOSTS = ["localhost", "127.0.0.1", "janssen-benedict-mentalhealthtracker.pbp.cs.ui.ac.id"]
 
 
 # Application definition
