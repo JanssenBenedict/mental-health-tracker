2658b98f350866c6a7963220aeb57cbf44a45909
JanssenBenedict <janssen.benedict@gmail.com>
2024-09-18 15:04:06 +0700

    Tutorial 3 Selesai

--- mental_health_tracker/settings.py
+++ mental_health_tracker/settings.py
@@ -1,3 +1,4 @@
+import os
 """
 Django settings for mental_health_tracker project.
 
@@ -23,7 +24,8 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-*dgkn!%$4*t8jvke4!uq&%2!&gb_bx_wze13(jg2)a+)@$o8a!'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+PRODUCTION = os.getenv("PRODUCTION", False)
+DEBUG = not PRODUCTION
 
 ALLOWED_HOSTS = ["localhost", "127.0.0.1", "janssen-benedict-mentalhealthtracker.pbp.cs.ui.ac.id"]
 
